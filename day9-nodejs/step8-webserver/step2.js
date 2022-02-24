980ccf2dd11bf42f7266a37638cc19751924269f
Vijay <34675810+trainingmaterial@users.noreply.github.com>
2022-02-24 16:12:22 +0530

    Update step2.js

--- day9-nodejs/step8-webserver/step2.js
+++ day9-nodejs/step8-webserver/step2.js
@@ -2,24 +2,23 @@ let http = require("http");
 let fs = require("fs");
 
 let server = http.createServer((req, res)=>{
-    // console.log(req.url);
-    if(req.url == "/"){
+    if(req.url == "favicon.ico"){
+        res.write("");
+    }else if(req.url == "/"){
         let htmldata = fs.readFileSync("./index.html","utf-8");
         res.write(htmldata);
+        res.end();
     }else{
-       fs.readFile("."+req.url+".html","utf-8", (error, data)=>{
-           // console.log(req.url+".html")
-           if(error){
-                let tempdata = fs.readFileSync("./notfound.html","utf-8");
-                res.write(tempdata);
-                console.log("Error ", error);
-            }else{
-                res.write(data);
+        fs.readFile("./"+req.url+".html","utf-8",function(err, data){
+            if(err){
+                res.write(fs.readFileSync("./notfound.html","utf-8")); 
+                res.end();
+           }else{
+               res.write(data);
+               res.end();
            }
-       });
-       
+       })
     }
-    res.end();
 });
 
 server.listen(2020,"localhost",()=>  console.log("server is now live on localhost:2020"))
